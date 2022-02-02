1c3a98fa0b98b32836394005ddd52d995d94bf3e
Nick M <mully7773@gmail.com>
2022-02-01 18:33:26 -0600

    Successfully rendered 'Hello, World' as html via handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
 router.get('/',  (req, res) => {
-    res.send("Hello World!");
-    // res.render('main')
+    res.render('index')
   });
 
   module.exports = router;
