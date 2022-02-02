e703151bcd0040caa27082ad723d1aace8850511
Nick M <mully7773@gmail.com>
2022-02-02 12:07:33 -0600

    added review route form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,4 +9,19 @@ router.get('/',  (req, res) => {
     res.render('review')
   });
 
+  router.get('/login',  (req, res) => {
+    res.render('login')
+  });
+
+  router.get('/signup',  (req, res) => {
+    res.render('signup')
+  });
+
+  router.get('/archives',  (req, res) => {
+    res.render('archives')
+  });
+
+
+
+
   module.exports = router;
