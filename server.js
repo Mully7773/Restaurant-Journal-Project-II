dcd925877fd380be8fbb78791108c2cf57436b7d
EricDeWald <eric.l.dewald@gmail.com>
2022-02-04 14:23:59 -0600

    Update server.js
    Imgur require

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const { ImgurClient } = require('imgur');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
